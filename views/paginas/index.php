a56879ee4c057531e7d1cc50a3924e57647363ca
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2022-04-15 21:25:04 +0200

    Ajustes carrusel Index

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -92,3 +92,103 @@
     </div>
 
 </section>
+
+<section>
+    <div class="segmentos">
+        <div class="areas">
+            <h2>Empleados Misionales</h2>
+            <img src="/build/img/misionales.webp" alt="Empleados Misionales"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus modi enim velit sed expedita ad qui repudiandae voluptate fuga ea. Unde, repellendus quaerat consequatur hic facilis praesentium recusandae temporibus ea!</p>
+        </div>
+        <div class="areas">
+            <h2>Responsabilidad Social Empresarial</h2>
+            <img src="/build/img/responsabilidadsocial.webp" alt="Responsabilidad Social Empresarial"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus modi enim velit sed expedita ad qui repudiandae voluptate fuga ea. Unde, repellendus quaerat consequatur hic facilis praesentium recusandae temporibus ea! </p>
+        </div>
+        <div class="areas">
+            <h2>Empleados Administrativos</h2>
+            <img src="/build/img/administrativos.webp" alt="Empleados Administrativos"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus modi enim velit sed expedita ad qui repudiandae voluptate fuga ea. Unde, repellendus quaerat consequatur hic facilis praesentium recusandae temporibus ea!</p>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="carrusel">
+        <h2>Confían en Nosotros</h2>
+
+        <div class="slide fade">
+            <h3>Alimentos y Bebidas</h3>
+            <img class="ayb" src="/build/img/westernwings.webp" alt="Logo Western Wings">
+            <img class="ayb" src="/build/img/mimos.webp" alt="Logo Mimos">
+            <img class="ayb" src="/build/img/dante.webp" alt="Logo Dante">
+            <img class="ayb" src="/build/img/cantina.webp" alt="Logo Cantina La 15">
+            <img class="ayb" src="/build/img/storia.webp" alt="Logo Storia D'amore">
+            <img class="ayb" src="/build/img/creta.webp" alt="Logo creta">
+            <img class="ayb" src="/build/img/deriva.webp" alt="Logo Deriva">
+
+            <ul class="circles">
+                    <li class="aybfondo"></li>
+                    <li class="aybfondo"></li>
+                    <li class="aybfondo"></li>
+                    <li class="aybfondo"></li>
+                    <li class="aybfondo"></li>
+            </ul>
+        </div>
+
+        <div class="slide fade">
+            <h3>Hotelería y Club</h3>
+            <img class="hotel" src="/build/img/estelar.webp" alt="Logo Hoteles Estelar">
+            <img class="hotel" src="/build/img/exe.webp" alt="Logo Hoteles Exe Bacatá">
+            <img class="hotel" src="/build/img/hilton.webp" alt="Logo Hoteles Hilton">
+            <img class="hotel" src="/build/img/nutibara.webp" alt="Logo Hotel Nutibara">
+            <img class="hotel" src="/build/img/clickclack.webp" alt="Logo Click Clack Hotel">
+            <img class="hotel" src="/build/img/courtyard.webp" alt="Logo Hotel Courtyard by Marriott">
+            <img class="hotel" src="/build/img/diez.webp" alt="Logo Hotel Diez">
+            <img class="hotel" src="/build/img/country.webp" alt="Logo Country Club">
+            <img class="hotel" src="/build/img/dann.webp" alt="Logo Hoteles Dann">
+            <img class="hotel" src="/build/img/atton.webp" alt="Logo Hoteles Atton">
+            <img class="hotel" src="/build/img/nh.webp" alt="Logo Hoteles Nh">
+            <img class="hotel" src="/build/img/union.webp" alt="Logo Club Unión Medellín">
+
+            <ul class="circles">
+                    <li class="hotelfondo"></li>
+                    <li class="hotelfondo"></li>
+                    <li class="hotelfondo"></li>
+                    <li class="hotelfondo"></li>
+                    <li class="hotelfondo"></li>
+            </ul>
+        </div>
+
+        <div class="slide fade">
+            <h3>Manufuctura, Call Center y Otros Sectores</h3>
+            <img class="otros" src="/build/img/superpack.webp" alt="Logo Superpack">
+            <img class="otros" src="/build/img/ppg.webp" alt="Logo PPG">
+            <img class="otros" src="/build/img/medicox.webp" alt="Logo Medicox">
+            <img class="otros" src="/build/img/hrmd.webp" alt="Logo HRMD">
+            <img class="otros" src="/build/img/frutafresca.webp" alt="Logo Fruta Fresca">
+
+            <ul class="circles">
+                    <li class="otrosfondo"></li>
+                    <li class="otrosfondo"></li>
+                    <li class="otrosfondo"></li>
+                    <li class="otrosfondo"></li>
+                    <li class="otrosfondo"></li>
+            </ul>
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">
+            <i class="fa fa-angle-left"></i>
+        </a>
+        <a class="next" onclick="plusSlides(1)">
+            <i class="fa fa-angle-right"></i>
+        </a>
+
+    </div>
+
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span> 
+        <span class="dot" onclick="currentSlide(2)"></span> 
+        <span class="dot" onclick="currentSlide(3)"></span> 
+    </div>
+<section>
